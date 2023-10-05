822e227cf7b4ad623927904c6a29d65b51d0b686
Joshuaj0209 <joshuaj0209@gmail.com>
2023-10-05 14:54:44 +0200

    Moved coin creation to file

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import {checkMovement} from "./collisionCheck";
 import {Vector3} from "three";
+import { createCoin } from './coinCreator.js';
+
 
 // Scene
 const scene = new THREE.Scene();
@@ -166,46 +168,10 @@ loader.load('models/villaHouse.glb', function (gltf) {
 
 let coins = []; // Array to store multiple coins
 
-function createCoin(x, y, z) {
-    const iconLoader = new GLTFLoader();
-
-    iconLoader.load('models/coin.glb', function (gltf) {
-        let coin = gltf.scene;
-        coin.position.set(x, y, z);
-        coin.scale.set(0.02, 0.02, 0.02);
-        scene.add(coin);
-
-        // Create a dummy mesh for the coin's BoxHelper
-        let iconBoxSize = new THREE.Vector3(0.2, 0.2, 0.2);
-        let coinDummyMesh = new THREE.Mesh(new THREE.BoxGeometry(iconBoxSize.x, iconBoxSize.y, iconBoxSize.z));
-
-        // Position this mesh at the position of the coin
-        coinDummyMesh.position.copy(coin.position);
-        let coinYOffset = 0;
-
-        coinDummyMesh.position.y += coinYOffset;
-
-        // Create a BoxHelper using this dummy mesh
-        let coinBoxHelper = new THREE.BoxHelper(coinDummyMesh, 0x00ff00);
-
-        // Add the BoxHelper to the scene
-        //scene.add(coinBoxHelper);
-
-        coins.push({
-            mesh: coin,
-            dummyMesh: coinDummyMesh,
-            boxHelper: coinBoxHelper,
-            collected: false
-        });
-    }, undefined, function (error) {
-        console.error(error);
-    });
-}
-
 // Create multiple coins
-createCoin(1, 0, 0);
-createCoin(-1, 0, 0);
-createCoin(0, 0, -1);
+createCoin(1, 0, 0, scene, coins);
+createCoin(-1, 0, 0, scene, coins);
+createCoin(0, 0, -1, scene, coins);
 
 // Animation function
 var cameraPosition;
