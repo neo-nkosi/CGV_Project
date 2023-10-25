f5ff79cf36fd95a99d70a9455d1d2b87cddcb67a
raees-wits <2322203@students.wits.ac.za>
2023-10-25 21:05:02 +0200

    light intensity reduced

--- lights.js
+++ lights.js
@@ -23,68 +23,68 @@ const createLights = (scene) => {
     const targetPosition = new THREE.Vector3(0, 0, 0);
 
     // Point light on staircase
-    const pointLightstairs = new THREE.SpotLight("red", 1, 2, Math.PI / 2);
+    const pointLightstairs = new THREE.SpotLight("red", 1, 1.5, Math.PI / 2);
     pointLightstairs.position.set(0.1, 0.7, 0.06);
     pointLightstairs.target.position.copy(targetPosition); // Set the light's target
     scene.add(pointLightstairs);
     scene.add(pointLightstairs.target); // Add the target object to the scene
 
-    // Point light on wall by staircase
-    const pointLightstairs2 = new THREE.SpotLight("red", 1, 2, Math.PI / 2);
+    // Point light on wall by wall1 by stairs
+    const pointLightstairs2 = new THREE.SpotLight("red", 1, 1.5, Math.PI / 2);
     pointLightstairs2.position.set(-1.8, 0.5, -0.06);
     pointLightstairs2.target.position.copy(targetPosition); // Set the light's target
     scene.add(pointLightstairs2);
     scene.add(pointLightstairs2.target); // Add the target object to the scene
-    // Point light on wall by staircase
-    const pointLightstairs3 = new THREE.SpotLight("red", 1, 2, Math.PI / 2);
+    // Point light on wall by wall2 by stairs
+    const pointLightstairs3 = new THREE.SpotLight("red", 1, 1.5, Math.PI / 2);
     pointLightstairs3.position.set(-2.4, 0.5, -3.01);
     pointLightstairs3.target.position.copy(targetPosition); // Set the light's target
     scene.add(pointLightstairs3);
     scene.add(pointLightstairs3.target); // Add the target object to the scene
-    // Point light on wall by staircase
-    const pointLightstairs4 = new THREE.SpotLight("red", 1, 2, Math.PI / 2);
+    // Point light on wall by inner wall 1 left
+    const pointLightstairs4 = new THREE.SpotLight("red", 1, 1.5, Math.PI / 2);
     pointLightstairs4.position.set(-2.7, 0.7, 0.62);
     pointLightstairs4.target.position.copy(targetPosition); // Set the light's target
     scene.add(pointLightstairs4);
     scene.add(pointLightstairs4.target); // Add the target object to the scene
 
-    // Point light on wall by staircase
-    const pointLightstairs5 = new THREE.SpotLight("red", 1, 2, Math.PI / 2);
+    // Point light on wall by inner wall 1 right
+    const pointLightstairs5 = new THREE.SpotLight("red", 1, 1.5, Math.PI / 2);
     pointLightstairs5.position.set(-5.4, 0.7, 1.18);
     pointLightstairs5.target.position.copy(targetPosition); // Set the light's target
     scene.add(pointLightstairs5);
     scene.add(pointLightstairs5.target); // Add the target object to the scene
 
-    // Point light on wall by staircase
-    const pointLightstairs6 = new THREE.SpotLight("red", 1, 2, Math.PI / 2);
+    // Point light on wall by inner doorway
+    const pointLightstairs6 = new THREE.SpotLight("red", 1, 1.5, Math.PI / 2);
     pointLightstairs6.position.set(-4.73, 0.6, -1.93);
     pointLightstairs6.target.position.copy(targetPosition); // Set the light's target
     scene.add(pointLightstairs6);
     scene.add(pointLightstairs6.target); // Add the target object to the scene
 
-     // Point light on wall by staircase
-     const pointLightstairs7 = new THREE.SpotLight("red", 1, 2, Math.PI / 2);
+     // Point light on wall by doorway right
+     const pointLightstairs7 = new THREE.SpotLight("red", 1, 1.5, Math.PI / 2);
      pointLightstairs7.position.set(-3.1, 0.6, -4.08);
      pointLightstairs7.target.position.copy(targetPosition); // Set the light's target
      scene.add(pointLightstairs7);
      scene.add(pointLightstairs7.target);
 
-      // Point light on wall by staircase
-      const pointLightstairs8 = new THREE.SpotLight("red", 1, 2, Math.PI / 4);
+      // Point light on wall by wall 3 adjacent
+      const pointLightstairs8 = new THREE.SpotLight("red", 1, 1.5, Math.PI / 4);
       pointLightstairs8.position.set(-6.25, 0.6, -0.10);
       pointLightstairs8.target.position.copy(targetPosition); // Set the light's target
       scene.add(pointLightstairs8);
       scene.add(pointLightstairs8.target);
 
-      // Point light on wall by staircase
-      const pointLightstairs9 = new THREE.SpotLight("red", 1, 2, Math.PI / 4);
+      // Point light on wall by back of wall 3
+      const pointLightstairs9 = new THREE.SpotLight("red", 1, 1.5, Math.PI / 4);
       pointLightstairs9.position.set(-8.4, 0.6, -1.11);
       pointLightstairs9.target.position.copy(targetPosition); // Set the light's target
       scene.add(pointLightstairs9);
       scene.add(pointLightstairs9.target);
 
-      // Point light on wall by staircase
-      const pointLightstairs1 = new THREE.SpotLight("red", 1, 2, Math.PI / 4);
+      // Point light on wall by fountain wall
+      const pointLightstairs1 = new THREE.SpotLight("red", 1, 1.5, Math.PI / 4);
       pointLightstairs1.position.set(-5.43, 0.6, -4.5);
       pointLightstairs1.target.position.copy(targetPosition); // Set the light's target
       scene.add(pointLightstairs1);
